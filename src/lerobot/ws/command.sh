7cf54f91409b1c6ccf3ad2e555fe1f083192f7e5
kevin010717 <k511153362@gmail.com>
2025-11-18 16:01:43 +0800

    1

---
+++ src/lerobot/ws/command.sh
@@ -0,0 +1,21 @@
+pip install datasets==2.19 # 解决 datasets 版本过高导致的问题
+pip install pytest # ==7.4.0 # 解决 pytest 版本过高导致的问题
+export HF_ENDPOINT=https://hf-mirror.com # 解决huggingface访问慢的问题
+
+
+# 数据采集经验
+# 1.单手操作
+# 2.夹爪回正
+# 3.先对准，再夹取
+# 4.下探过程缓慢
+
+# todo:
+# 用checkpoint推理
+# 1.lerobot 0.4.1迁移
+# 2.pi groot数据采集，训练，推理
+# 3.t700机器人数据采集系统搭建                  11.31开工
+# 4.vr，视觉遥操系统搭建，关联仿真，产线数据采集，数据增强
+# 5.openvla openpi opengalaxea框架
+# 6.VLN
+
+
